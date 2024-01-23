714d43581093a749075a5d3859e780af9073a551
Andres Vazquez <andres.vazquez.martin@gmail.com>
2024-01-23 21:07:41 +0100

    fetching using suspender

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import React, { Suspense } from 'react';
+import { fetchData } from './fetchData';
 import './App.css';
 
-function App() {
-  return <h1>Fetching API</h1>;
-}
+const apiData = fetchData('https://jsonplaceholder.typicode.com/todos');
 
+function App() {
+  const data = apiData.read();
+
+  return (
+    <div className="App">
+      <h1>Fetch like a Pro</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ul className="card">
+          {data?.map((item) => (
+            <li key={item.id}>{item.title}</li>
+          ))}
+        </ul>
+      </Suspense>
+    </div>
+  );
+}
 export default App;
